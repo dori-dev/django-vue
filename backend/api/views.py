ee2d28c2abd3c9f7d3afc68e398c78df25f5339b
dori-dev <mr.dori.dev@gmail.com>
2023-04-16 11:00:26 +0330

    use permissions

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics
+from rest_framework import permissions
 
 from blog import models
 from api import serializers
@@ -21,9 +22,15 @@ class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
 class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
+    permission_classes = [
+        permissions.IsAdminUser,
+    ]
 
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
     lookup_field = "username"
+    permission_classes = [
+        permissions.IsAdminUser,
+    ]

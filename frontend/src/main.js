7a48e03336a60f3a5ce4502ddde8a26e6792960c
dori-dev <mr.dori.dev@gmail.com>
2023-04-21 21:24:16 +0330

    add axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import axios from "axios";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -7,3 +8,5 @@ import "popper.js/dist/popper.min.js";
 import "bootstrap/dist/js/bootstrap.min.js";
 
 createApp(App).use(store).use(router).mount("#app");
+
+axios.defaults.baseURL = "http://127.0.0.1:8000";
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from "vuex";
+import axios from "axios";
 
 export default createStore({
   state: {
@@ -11,9 +12,11 @@ export default createStore({
       if (token) {
         state.isAuthenticated = true;
         state.token = token;
+        axios.defaults.headers.common["Authorization"] = `Token ${token}`;
       } else {
         state.isAuthenticated = false;
         state.token = "";
+        axios.defaults.headers.common["Authorization"] = "";
       }
     },
     login(state, token) {
@@ -21,16 +24,19 @@ export default createStore({
         state.isAuthenticated = true;
         state.token = token;
         localStorage.setItem("token", token);
+        axios.defaults.headers.common["Authorization"] = `Token ${token}`;
       } else {
         state.isAuthenticated = false;
         state.token = "";
         localStorage.removeItem("token");
+        axios.defaults.headers.common["Authorization"] = "";
       }
     },
     logout(state) {
       state.isAuthenticated = false;
       state.token = "";
       localStorage.removeItem("token");
+      axios.defaults.headers.common["Authorization"] = "";
     },
   },
 });

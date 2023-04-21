ce3a755d4d2d2ce77133e0cd9f3d75ff6d94e200
dori-dev <mr.dori.dev@gmail.com>
2023-04-21 22:18:53 +0330

    add endpoint for logout

--- frontend/src/views/Logout.vue
+++ frontend/src/views/Logout.vue
@@ -1,17 +1,45 @@
 <template>
   <div class="login">
-    <h1>Logout</h1>
+    <h3 class="mb-4">Are you sure to logout?</h3>
+    <button
+      class="btn btn-secondary me-3"
+      @click="this.$router.push('/profile')"
+    >
+      Cancle
+    </button>
     <button class="btn btn-danger" @click="doLogout">Logout</button>
+    <br />
+    <div
+      class="alert alert-danger mt-5 mx-auto px-3"
+      style="width: fit-content"
+      v-if="logoutErr"
+    >
+      {{ logoutErr }}
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Logout",
+  data() {
+    return {
+      logoutErr: "",
+    };
+  },
   methods: {
     doLogout() {
-      this.$store.commit("logout");
-      this.$router.push("/");
+      axios
+        .post("/api/auth/token/logout/")
+        .then((response) => {
+          this.$store.commit("logout");
+          this.$router.push("/");
+        })
+        .catch((error) => {
+          this.logoutErr = "Something wen't wrong to logging out you.";
+        });
     },
   },
 };

ff0c8d008a948fc82c419de2b69aafae86992267
dori-dev <mr.dori.dev@gmail.com>
2023-04-21 18:57:28 +0330

    set slug automatically

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,6 @@
+from random import choices
+from string import ascii_letters
+
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils.timezone import now
@@ -40,7 +43,16 @@ class Article(models.Model):
 
     def save(self, *args, **kwargs) -> None:
         self.title = self.title.title()
+        if not self.slug:
+            slug = self._random_slug(8)
+            while Article.objects.filter(slug=slug).exists():
+                slug = self._random_slug(8)
+            self.slug = slug
         return super().save(*args, **kwargs)
+
+    @staticmethod
+    def _random_slug(length: int) -> str:
+        return "".join(choices(ascii_letters, k=length))
 
     def __str__(self) -> str:
         return self.title

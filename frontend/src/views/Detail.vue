daad5dcf5b0f9e538c76bb504452e78c3cd385ba
dori-dev <mr.dori.dev@gmail.com>
2023-04-22 08:31:03 +0330

    use endpoint in detail page

--- frontend/src/views/Detail.vue
+++ frontend/src/views/Detail.vue
@@ -1,133 +1,136 @@
 <template>
-  <div class="home text-start">
-    <h1 class="mb-4 text-center">{{ article.title }}</h1>
-    <article>
-      <div>
-        {{ article.description }}
-      </div>
-      <div>
-        Write by <span class="badge bg-primary">{{ article.author }}</span>
-      </div>
-      <div
-        class="col-md-5 col-12 d-flex justify-content-start mt-3"
-        v-if="$store.state.isAuthenticated"
-      >
-        <button class="btn btn-primary btn-sm me-3 col-5" @click="changeEdit">
-          <svg
-            class="btn-icon"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-          >
-            <path
-              d="M497.9 74.16L437.8 14.06c-18.75-18.75-49.19-18.75-67.93 0l-56.53 56.55l127.1 128l56.56-56.55C516.7 123.3 516.7 92.91 497.9 74.16zM290.8 93.23l-259.7 259.7c-2.234 2.234-3.755 5.078-4.376 8.176l-26.34 131.7C-1.921 504 7.95 513.9 19.15 511.7l131.7-26.34c3.098-.6191 5.941-2.141 8.175-4.373l259.7-259.7L290.8 93.23z"
-            />
-          </svg>
-          Edit
-        </button>
-        <button class="btn btn-danger btn-sm col-5" @click="removeArticle">
-          <svg
-            class="btn-icon"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-          >
-            <path
-              d="M32 464C32 490.5 53.5 512 80 512h288c26.5 0 48-21.5 48-48V128H32V464zM304 208C304 199.1 311.1 192 320 192s16 7.125 16 16v224c0 8.875-7.125 16-16 16s-16-7.125-16-16V208zM208 208C208 199.1 215.1 192 224 192s16 7.125 16 16v224c0 8.875-7.125 16-16 16s-16-7.125-16-16V208zM112 208C112 199.1 119.1 192 128 192s16 7.125 16 16v224C144 440.9 136.9 448 128 448s-16-7.125-16-16V208zM432 32H320l-11.58-23.16c-2.709-5.42-8.25-8.844-14.31-8.844H153.9c-6.061 0-11.6 3.424-14.31 8.844L128 32H16c-8.836 0-16 7.162-16 16V80c0 8.836 7.164 16 16 16h416c8.838 0 16-7.164 16-16V48C448 39.16 440.8 32 432 32z"
-            />
-          </svg>
-          Remove
-        </button>
-      </div>
-      <hr />
-      <div v-if="!edit" style="white-space: pre-line">
-        {{ article.content }}
-      </div>
-    </article>
-  </div>
-  <div class="row d-flex justify-content-start" v-if="edit">
-    <div class="col-md-8 col-12">
-      <form @submit.prevent="updateArticle">
-        <div class="inputs mb-4">
-          <div class="group">
-            <input
-              type="text"
-              name="title"
-              id="title"
-              class="input form-control"
-              v-model="title"
-              :class="{
-                'is-invalid': titleErr === true,
-                'is-valid': titleErr === false,
-              }"
-            />
-            <label for="title" class="input-label title fill-input-label"
-              >Title</label
-            >
-            <div class="invalid-feedback text-start" v-if="titleErr">
-              {{ titleMsg }}
-            </div>
-          </div>
-          <div class="group mt-3">
-            <input
-              type="text"
-              name="description"
-              id="description"
-              class="input form-control"
-              v-model="description"
-              :class="{
-                'is-invalid': descriptionErr === true,
-                'is-valid': descriptionErr === false,
-              }"
-            />
-            <label
-              for="description"
-              class="input-label description fill-input-label"
-              >Description</label
-            >
-            <div class="invalid-feedback text-start" v-if="descriptionErr">
-              {{ descriptionMsg }}
-            </div>
-          </div>
-          <div class="group mt-3">
-            <textarea
-              class="input form-control"
-              name="content"
-              id="content"
-              rows="10"
-              v-model="content"
-              :class="{
-                'is-invalid': contentErr === true,
-                'is-valid': contentErr === false,
-              }"
-            >
-            </textarea>
-            <label for="content" class="input-label content fill-input-label"
-              >Content</label
-            >
-            <div class="invalid-feedback text-start" v-if="contentErr">
-              {{ contentMsg }}
-            </div>
-          </div>
+  <div class="content" v-if="!articleError">
+    <div class="home text-start">
+      <h1 class="mb-4 text-center">{{ article.title }}</h1>
+      <article>
+        <div>
+          {{ article.description }}
         </div>
-        <button class="btn btn-primary w-75 mt-3 mb-5">Update Article</button>
-      </form>
+        <div>
+          Write by <span class="badge bg-primary">{{ article.author }}</span>
+        </div>
+        <div
+          class="col-md-5 col-12 d-flex justify-content-start mt-3"
+          v-if="$store.state.isAuthenticated"
+        >
+          <button class="btn btn-primary btn-sm me-3 col-5" @click="changeEdit">
+            <svg
+              class="btn-icon"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path
+                d="M497.9 74.16L437.8 14.06c-18.75-18.75-49.19-18.75-67.93 0l-56.53 56.55l127.1 128l56.56-56.55C516.7 123.3 516.7 92.91 497.9 74.16zM290.8 93.23l-259.7 259.7c-2.234 2.234-3.755 5.078-4.376 8.176l-26.34 131.7C-1.921 504 7.95 513.9 19.15 511.7l131.7-26.34c3.098-.6191 5.941-2.141 8.175-4.373l259.7-259.7L290.8 93.23z"
+              />
+            </svg>
+            Edit
+          </button>
+          <button class="btn btn-danger btn-sm col-5" @click="removeArticle">
+            <svg
+              class="btn-icon"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+            >
+              <path
+                d="M32 464C32 490.5 53.5 512 80 512h288c26.5 0 48-21.5 48-48V128H32V464zM304 208C304 199.1 311.1 192 320 192s16 7.125 16 16v224c0 8.875-7.125 16-16 16s-16-7.125-16-16V208zM208 208C208 199.1 215.1 192 224 192s16 7.125 16 16v224c0 8.875-7.125 16-16 16s-16-7.125-16-16V208zM112 208C112 199.1 119.1 192 128 192s16 7.125 16 16v224C144 440.9 136.9 448 128 448s-16-7.125-16-16V208zM432 32H320l-11.58-23.16c-2.709-5.42-8.25-8.844-14.31-8.844H153.9c-6.061 0-11.6 3.424-14.31 8.844L128 32H16c-8.836 0-16 7.162-16 16V80c0 8.836 7.164 16 16 16h416c8.838 0 16-7.164 16-16V48C448 39.16 440.8 32 432 32z"
+              />
+            </svg>
+            Remove
+          </button>
+        </div>
+        <hr />
+        <div v-if="!edit" style="white-space: pre-line">
+          {{ article.content }}
+        </div>
+      </article>
     </div>
+    <div class="row d-flex justify-content-start" v-if="edit">
+      <div class="col-md-8 col-12">
+        <form @submit.prevent="updateArticle">
+          <div class="inputs mb-4">
+            <div class="group">
+              <input
+                type="text"
+                name="title"
+                id="title"
+                class="input form-control"
+                v-model="title"
+                :class="{
+                  'is-invalid': titleErr === true,
+                  'is-valid': titleErr === false,
+                }"
+              />
+              <label for="title" class="input-label title fill-input-label"
+                >Title</label
+              >
+              <div class="invalid-feedback text-start" v-if="titleErr">
+                {{ titleMsg }}
+              </div>
+            </div>
+            <div class="group mt-3">
+              <input
+                type="text"
+                name="description"
+                id="description"
+                class="input form-control"
+                v-model="description"
+                :class="{
+                  'is-invalid': descriptionErr === true,
+                  'is-valid': descriptionErr === false,
+                }"
+              />
+              <label
+                for="description"
+                class="input-label description fill-input-label"
+                >Description</label
+              >
+              <div class="invalid-feedback text-start" v-if="descriptionErr">
+                {{ descriptionMsg }}
+              </div>
+            </div>
+            <div class="group mt-3">
+              <textarea
+                class="input form-control"
+                name="content"
+                id="content"
+                rows="10"
+                v-model="content"
+                :class="{
+                  'is-invalid': contentErr === true,
+                  'is-valid': contentErr === false,
+                }"
+              >
+              </textarea>
+              <label for="content" class="input-label content fill-input-label"
+                >Content</label
+              >
+              <div class="invalid-feedback text-start" v-if="contentErr">
+                {{ contentMsg }}
+              </div>
+            </div>
+          </div>
+          <button class="btn btn-primary w-75 mt-3 mb-5">Update Article</button>
+        </form>
+      </div>
+    </div>
+  </div>
+  <div class="alert alert-danger" v-if="articleError">
+    Article not found (404)
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Detail",
   data() {
-    let articles = localStorage.getItem("articles");
-    articles = JSON.parse(articles);
-    let article = articles.find(
-      (element) => element.slug == this.$route.params.slug
-    );
     return {
-      article: article,
-      title: article.title,
-      description: article.description,
-      content: article.content.trim(),
+      article: {},
+      articleError: false,
+      title: "",
+      description: "",
+      content: "",
       titleErr: null,
       descriptionErr: null,
       contentErr: null,
@@ -137,8 +140,21 @@ export default {
       edit: false,
     };
   },
+  mounted() {
+    axios
+      .get(`/api/article/${this.$route.params.slug}/`)
+      .then((response) => {
+        this.article = response.data;
+        this.title = this.article.title;
+        this.description = this.article.description;
+        this.content = this.article.content;
+      })
+      .catch((error) => {
+        this.articleError = true;
+      });
+  },
   methods: {
-    mounted() {
+    setInputEvent() {
       let inputs = document.querySelectorAll(".input");
       inputs.forEach((input) => {
         input.addEventListener("input", (e) => {
@@ -153,7 +169,7 @@ export default {
     changeEdit() {
       this.edit = !this.edit;
       if (this.edit) {
-        setTimeout(this.mounted, 500);
+        setTimeout(this.setInputEvent, 500);
       }
     },
     updateArticle() {
